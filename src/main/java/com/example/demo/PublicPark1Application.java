2dff71d5b1b65f99cf3402f971aa664e0b051db3
chaseunghan <cha7516@naver.com>
2024-01-16 23:07:48 +0900

    환경세팅 완료

---
+++ src/main/java/com/example/demo/PublicPark1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicPark1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicPark1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/PublicPark1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicPark1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
